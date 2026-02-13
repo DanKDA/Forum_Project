d806896aa4f4e6bad211765163eb2744a7a3b985
GheorgheKDA <mihalachigheorghe05@gmail.com>
2026-02-13 16:28:26 +0200

    Nu merge

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <h1>Duhvffdeb</h1>
       <h1>Gheorghe bos</h1>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 67)}>
           count is {count}
         </button>
         <p>
